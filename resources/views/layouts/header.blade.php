ed0e0d22d269fbc8e71e9c70fb146291a0f97f1e
remoteexecutive <remoteexecutive@gmail.com>
2016-04-14 03:01:14 +0800

    Merged Project manager with Job.tc Hiring System, Found bugs in existing implementation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,7 +31,7 @@
                                          title="My profile"
                                     />
                                 </li>
-                                <li><a class="btn btn-link" href="/logout">Log out</a></li>
+                                <li><a class="btn btn-link" href="/laravel-pm/logout">Log out</a></li>
                             </ul>
                         </li>
                     </ul>
