c3a0834ff225f9eec92a3da3783a7ddb42ec96bc
Larynill Ances <larynillances@gmail.com>
2016-05-06 17:19:19 +0800

    Navbar links update version 2

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -88,3 +88,18 @@
         <i class="fa fa-globe"></i>
         <span>CSS</span></a>
 </li>
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle">
+        <i class="glyphicon glyphicon-user"></i>
+        <span>{{ Auth::user()->username }} <i class="caret"></i></span>
+    </a>
+    <ul class="dropdown-menu">
+        <li>
+            <a href="{{ url('/profile') }}"><i class="glyphicon glyphicon-user"></i> My Profile</a>
+        </li>
+        <li role="separator" class="divider"></li>
+        <li>
+            <a href="{{ url('/logout') }}"><i class="glyphicon glyphicon-off"></i> Logout</a>
+        </li>
+    </ul>
+</li>
