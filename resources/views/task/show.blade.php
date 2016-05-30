e3d0cf0230e626980c7d4a137ff15f5fbcd6022c
Larynill Ances <larynillances@gmail.com>
2016-05-30 18:54:55 +0800

    Remove "X" button from task header

--- resources/views/task/show.blade.php
+++ resources/views/task/show.blade.php
@@ -48,7 +48,7 @@
                                             <input type="hidden" class="task_list_id" value="{{$task->task_id}}" />
 
                                             <a href="#" class="drag-handle icon icon-btn move-tasklist"><i class="fa fa-arrows"></i></a>&nbsp;&nbsp;&nbsp;
-                                            <a href="#" class="icon icon-btn alert_delete"><i class="fa fa-times" aria-hidden="true"></i></a>
+                                            {{--<a href="#" class="icon icon-btn alert_delete"><i class="fa fa-times" aria-hidden="true"></i></a>--}}
                                             <input type="hidden" class="task_list_item_id" value="{{$list_item->id}}" />
                                             <input type="hidden" class="task_list_id" value="{{$task->task_id}}" />
                                         </div>
