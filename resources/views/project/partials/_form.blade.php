0da8046f74a8af07ff1bc9a6310f4d2377717577
Larynill Ances <larynillances@gmail.com>
2016-05-31 19:15:20 +0800

    CSS and layout update for 05-31-2016 version 1

--- resources/views/project/partials/_form.blade.php
+++ resources/views/project/partials/_form.blade.php
@@ -4,7 +4,8 @@
             {!!  Form::select('project_type', [
                 'Standard' => 'Standard',
                 'Hiring Assessment' => 'Hiring Assessment',
-                'Software Development' => 'Software Development'
+                'Software Development' => 'Software Development',
+                'Coding' => 'Coding'
             ], isset($project->project_type) ? $project->project_type : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Project Type', 'tabindex' => '7'] )  !!}
         </div>
     </div>
@@ -17,7 +18,7 @@
     <div class="form-group">
         <div class="col-md-12">
             {!!  Form::textarea('project_description',isset($project->project_description) ?
-            $project->project_description : '',['size' => '30x3', 'class' => 'form-control', 'placeholder' => 'Project Description', 'tabindex' => '6']) !!}
+            $project->project_description : '',['rows' => '3','class' => 'form-control', 'placeholder' => 'Project Description', 'tabindex' => '6']) !!}
         </div>
     </div>
     <div class="form-group">
@@ -27,21 +28,7 @@
             $clients = App\Models\Company::orderBy('name', 'asc')->lists('name', 'id');
             ?>
             {!! Form::select('company_id', $clients, isset($project->company_id) ?
-            $project->client_id : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Company Name', 'tabindex' =>'2'] )  !!}
-        </div>
-    </div>
-    <div class="form-group">
-        <div class="col-md-6">
-            {!!  Form::input('text','account',isset($project->account) ?
-            $project->account : '', ['class' => 'form-control form-control-inline input-medium', 'placeholder' => 'Account', 'tabindex' => '4'])  !!}
-        </div>
-        <div class="col-md-6">
-            {!!  Form::select('currency', [
-                'USD' => 'USD',
-                'EUR' => 'EUR',
-                'GBP' => 'GBP',
-                'PHP' => 'PHP'
-            ], isset($project->currency) ? $project->currency : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Currency', 'tabindex' => '7'] )  !!}
+            $project->client_id : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Company', 'tabindex' =>'2'] )  !!}
         </div>
     </div>
     <div class="form-group">
@@ -55,15 +42,26 @@
         </div>
     </div>
     <div class="form-group">
-        <div class="col-md-6">
+        <div class="col-md-2">
+            {!! Form::input('text','rate_value',isset($project->rate_value) ? $project->rate_value : '',['class' =>
+            'form-control', 'placeholder' => 'Rate', 'tabindex' => '8']) !!}
+        </div>
+        <div class="col-md-3">
+            {!!  Form::select('currency', [
+                'USD' => 'USD',
+                'EUR' => 'EUR',
+                'GBP' => 'GBP',
+                'PHP' => 'PHP'
+            ], isset($project->currency) ? $project->currency : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Currency', 'tabindex' => '7'] )  !!}
+        </div>
+        <div class="col-md-3">
             {!!  Form::select('rate_type', [
                 'fixed' => 'Fixed',
                 'hourly' => 'Hourly'
             ], isset($project->rate_type) ? $project->rate_type : '', ['class' => 'form-control input-xlarge select2me', 'placeholder' => 'Select Rate', 'tabindex' => '7'] )  !!}
         </div>
-        <div class="col-md-6">
-            {!! Form::input('text','rate_value',isset($project->rate_value) ? $project->rate_value : '',['class' =>
-            'form-control', 'placeholder' => 'Hourly Rate', 'tabindex' => '8']) !!}
+        <div class="col-md-4">
+            {!!  Form::input('text','account',isset($project->account) ? $project->account : '', ['class' => 'form-control form-control-inline input-medium', 'placeholder' => 'Account', 'tabindex' => '4'])  !!}
         </div>
     </div>
     <div class="row">
