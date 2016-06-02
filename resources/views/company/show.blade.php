d830d5030ce67015ddf90fb92549e20a9e1213c3
remoteexecutive <remoteexecutive@gmail.com>
2016-06-02 20:15:55 +0800

    Applicant to User Implemented

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.default')
 @section('content')
 <ul id="company_tabs" class="nav nav-tabs">
-    <li class="active"><a data-toggle="pill" href="#my_tasks">My Tasks</a></li>
+    <li class="active"><a data-toggle="pill" href="#my_tasks">Projects</a></li>
     @if(Auth::user('user')->level() === 1)
     <li><a data-toggle="pill" href="#assign_projects">Assign Projects</a></li>
     <li><a data-toggle="pill" href="#assign_tests">Assign Tests</a></li>
