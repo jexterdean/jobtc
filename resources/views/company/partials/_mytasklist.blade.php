1f541a86d5b36d22d53f12b5280f4e60c8405689
remoteexecutive <remoteexecutive@gmail.com>
2016-06-22 20:23:14 +0800

    Company Project Sharing Label

--- resources/views/company/partials/_mytasklist.blade.php
+++ resources/views/company/partials/_mytasklist.blade.php
@@ -6,12 +6,16 @@
             <div class="box-container">
                 <div class="box-header toggle-subprojects" id="project-{{$project->project_id}}" data-toggle="collapse" data-target="#project-collapse-{{ $project->project_id }}">
                     <h3 class="box-title">{{$project->project_title}}</h3>
-                     <div class="pull-right">
+                    <div class="pull-right">
                         @if(intval($project->company_id) !== intval($company_id))
-                        <label>Shared by {{$project->company->name}}</label>
+                        <div class="row">
+                            <label>Shared by {{$project->company->name}}</label>
+                        </div>
                         @endif
                         @if(Auth::user('user')->user_id !== $project->user_id)
+                        <div class="row">
                         <label>Shared by {{$project->user->name}}</label>
+                        </div>
                         @endif
                     </div>
                     <input class="project_id" type="hidden" value="{{$project->project_id}}"/>
@@ -19,7 +23,7 @@
                 </div>
                 <div class="box-body">
                     <div id="project-collapse-{{ $project->project_id }}" class="box-content collapse">
-                      
+
                     </div><!--Box Container-->
                 </div>
             </div>
