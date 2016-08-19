b27662e7e9bfc831656ab93915d67c0c888bf090
remoteexecutive <remoteexecutive@gmail.com>
2016-08-19 20:17:32 +0800

    Email for Reset Password

--- resources/views/user/forgotPassword.blade.php
+++ resources/views/user/forgotPassword.blade.php
@@ -2,4 +2,4 @@ Hello {{ $email }},
 
 Click here to change your password:
 
-<a target="_blank" href="https://{{$url}}/resetPassword/?token={{$token_str}}&usertype={{$usertype}}">Reset Password</a>
+<a target="_blank" href="https://job.tc/pm/resetPassword/?token={{$token_str}}&usertype={{$usertype}}">Reset Password</a>
